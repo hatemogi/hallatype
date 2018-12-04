358023765fcc6c5639a93c355452af978d598913
Daehyun Kim <hatemogi@gmail.com>
2018-12-05 00:45:21 +0900

    캔버스와 비트맵 준비

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <canvas id="캔버스" width="640" height="480"></canvas>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
@@ -14,5 +15,15 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+
+export default class Home extends Vue {
+  public mounted() {
+    const canvas = document.getElementById('캔버스') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+    ctx.fillStyle = 'rgb(200, 0, 0)';
+    ctx.strokeStyle = '1px rgb(0, 0, 0)';
+    ctx.strokeRect(0, 0,  639, 479);
+    ctx.fillRect(10, 10, 50, 50);
+  }
+}
 </script>
