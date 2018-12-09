2491068369fcea71ddbb451b10fc23d65472892e
Daehyun Kim <hatemogi@gmail.com>
2018-12-09 13:03:55 +0900

    글꼴 그릴 때 배경 유지 (투명) 처리

--- src/graphics.ts
+++ src/graphics.ts
@@ -9,10 +9,10 @@ export default class Graphic {
 
     public draw() {
         const ctx = this.ctx;
-        ctx.fillStyle = 'rgb(0, 0, 150)';
+        ctx.fillStyle = 'rgb(180, 180, 255)';
         ctx.strokeStyle = '1px rgb(0, 0, 0)';
         ctx.strokeRect(0, 0, 639, 350);
-        // ctx.fillRect(0, 0, 50, 50);
+        ctx.fillRect(0, 0, 150, 48);
         const 검정 = [0, 0, 0, 255];
         this.drawChar(20, 20, '한'.charCodeAt(0), 검정);
         this.drawChar(20 + 16, 20, '글'.charCodeAt(0), 검정);
@@ -22,32 +22,30 @@ export default class Graphic {
 
     private drawChar(x: number, y: number, char: number, rgba: number[]): number {
         const [width, bitmap] = charToBitmap(char);
-        const image = this.bitmapToImageData(width, bitmap, rgba);
+        const image = this.bitmapToImageData(x, y, width, bitmap, rgba);
         this.ctx.putImageData(image, x, y);
         return width;
     }
 
     private drawText(x: number, y: number, text: string, rgba: number[]) {
         let px = x;
-        for (let i of text) {
+        for (const i of text) {
             px += this.drawChar(px, y, i.charCodeAt(0), rgba);
         }
     }
 
-    private bitmapToImageData(width: number, bitmap: number[], rgba: number[]): ImageData {
+    private bitmapToImageData(x: number, y: number, width: number, bitmap: number[], rgba: number[]): ImageData {
         const height = bitmap.length;
-        const image = this.ctx.createImageData(width, height);
-        for (let y = 0; y < height; y++) {
+        const image = this.ctx.getImageData(x, y, width, height);
+        for (let py = 0; py < height; py++) {
             let bit = 1 << width;
-            for (let x = 0; x < width; x++) {
+            for (let px = 0; px < width; px++) {
                 bit = bit >>> 1;
-                const i = (y * width + x) * 4;
-                if ((bitmap[y] & bit) > 0) {
+                const i = (py * width + px) * 4;
+                if ((bitmap[py] & bit) > 0) {
                     for (let t = 0; t < 4; t++) {
                         image.data[i + t] = rgba[t];
                     }
-                } else {
-                    image.data[i + 3] = 0;
                 }
             }
         }
