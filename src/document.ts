28af2576de6165b8172880fe4f6ea45e29358b0b
김대현 <hatemogi@gmail.com>
2018-12-26 20:48:40 +0900

    글자 모델 여전히 고민중

--- src/document.ts
+++ src/document.ts
@@ -11,38 +11,50 @@
  */
 
 import * as color from './color';
+import {decompose} from './hangul';
 
 enum 글자종류 {
-    한글, 라틴,
+    한글, 라틴, 모름,
 }
 
 enum 글자상태 {
     지문,
     정타,
     오타,
+    정정,
     조립중,
 }
 
+function 글자종류판단(code: number): 글자종류 {
+    if (code < 256) {
+        return 글자종류.라틴;
+    } else if (code >= 0xAC00 && code <= 0xD7AF) {
+        return 글자종류.한글;
+    } else {
+        return 글자종류.모름;
+    }
+}
+
 class 글자 {
-    public readonly type: 글자종류;
-    public readonly code: number;
-    constructor(type: 글자종류, code: number) {
-        this.type = type;
-        this.code = code;
+    public readonly 종류: 글자종류;
+    public readonly codes: number[];
+    constructor(종류: 글자종류, codes: number[]) {
+        this.종류 = 종류;
+        this.codes = codes;
     }
 
     // 반각 or 전각 글자 구분
     get double(): boolean {
-        return this.type === 글자종류.한글;
+        return this.종류 === 글자종류.한글;
     }
 }
 
 function 한글(code: number) {
-    return new 글자(글자종류.한글, code);
+    return new 글자(글자종류.한글, decompose(code));
 }
 
 function 라틴(code: number) {
-    return new 글자(글자종류.라틴, code);
+    return new 글자(글자종류.라틴, [code]);
 }
 
 class TextAttribute {
@@ -77,10 +89,18 @@ export interface TextModelDrawer {
     onTextFill: (pos: TextPosition, double: boolean, fill: color.RGBA) => void;
 }
 
+/**
+ * 특징: 지문과 입력문이 있다. 지문은 한번에 다 쓰고 바꾸지 않고,
+ * 입력문은 순차적으로 쓰되 앞뒤로 한글자 단위로만 이동한다.
+ * 지문은 특정 영역을 한꺼번에 화면에 보이고,
+ * 입력문은 지문을 쫓아가며 상태를 바꾼다.
+ * 입력문과 지문의 차이가 나는 범위는 한계가 있다. (예. 최대 5글자)
+ */
 export class TextModel {
     private cursor: TextPosition = new TextPosition(0, 0);
     private 기본속성 = new TextAttribute();
     private lines: TextCharactor[][] = [];
+    private maxColumn = 80;
     private currentLine: TextCharactor[] = this.lines[0];
     private drawer?: TextModelDrawer;
     constructor(drawer?: TextModelDrawer) {
@@ -94,10 +114,27 @@ export class TextModel {
     public write(text: string): void {
         // noop yet;
         // this.listeners.forEach
+        for (let i = 0; i <= text.length; i++) {
+            if (text.codePointAt(i)) {
+                this.writeChar(text.codePointAt(i)!);
+            }
+        }
     }
 
     public refresh(lines: [number, number]): void {
         // 특정 영역 다시 그리기
+    }
+
+    private writeChar(code: number): void {
+        // noop
+        switch (글자종류판단(code)) {
+            case 글자종류.라틴:
+                break;
+            case 글자종류.한글:
+                break;
+            case 글자종류.모름:
+                break;
+        }
     }
 }
 
