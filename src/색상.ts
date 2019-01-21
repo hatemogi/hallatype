4c98f888b410864bd5a017c168e028235cdf8560
김대현 <hatemogi@gmail.com>
2019-01-21 21:20:41 +0900

    색상 변경

--- src/색상.ts
+++ src/색상.ts
@@ -1,10 +1,29 @@
 export type RGBA = [number, number, number, number];
 
 export const 흰색: RGBA = [255, 255, 255, 255];
-export const 밝은회색: RGBA = [180, 180, 180, 255];
-export const 회색: RGBA = [128, 128, 128, 255];
+export const 밝은회색: RGBA = [0x65, 0x7b, 0x83, 255];
+export const 회색: RGBA = [0x07, 0x36, 0x42, 255];
 export const 검정: RGBA = [0, 0, 0, 255];
-export const 빨강: RGBA = [200, 64, 64, 255];
-export const 초록: RGBA = [16, 128, 16, 255];
-export const 파랑: RGBA = [0, 0, 255, 255];
+export const 빨강: RGBA = [0xD3, 0x36, 0x82, 255];
+export const 초록: RGBA = [0x85, 0x99, 0, 255];
+export const 파랑: RGBA = [0x26, 0x8d, 0xd2, 255];
 export const 노랑: RGBA = [255, 255, 0, 255];
+
+/*
+$base03:    #002b36;
+$base02:    #073642;
+$base01:    #586e75;
+$base00:    #657b83;
+$base0:     #839496;
+$base1:     #93a1a1;
+$base2:     #eee8d5;
+$base3:     #fdf6e3;
+$yellow:    #b58900;
+$orange:    #cb4b16;
+$red:       #dc322f;
+$magenta:   #d33682;
+$violet:    #6c71c4;
+$blue:      #268bd2;
+$cyan:      #2aa198;
+$green:     #859900;
+*/
